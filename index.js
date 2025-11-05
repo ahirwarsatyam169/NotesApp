3af6e85d87b72731f37975d4e53d04cd5573c4b1
ahirwarsatyam169 <ahirwarsatyam169@gmail.com>
2025-11-05 10:17:53 +0530

    Enhance button styles with font size adjustments
    Added font size change on button click and mouseover events.

--- index.js
+++ index.js
@@ -35,8 +35,21 @@ let btndelete = document.querySelector("#btndelete")
 btndelete.addEventListener("click", function(){
     localStorage.removeItem("titleval") 
     localStorage.removeItem("textarea")
-
+    
     title.value = ""
     text.value = ""
-
+    
+})
+btnsave.addEventListener("click", function(){
+    btnsave.style.fontSize = "x-small";
+})
+btndelete.addEventListener("click",function(){
+    btndelete.style.fontSize = "x-small"
+    
+})
+window.addEventListener("mouseover",function(){
+    btndelete.style.fontSize = "initial"
+})
+window.addEventListener("mouseover",function(){
+    btnsave.style.fontSize = "initial"
 })
